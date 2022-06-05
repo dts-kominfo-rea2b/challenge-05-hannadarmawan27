623accee4975aaca105474d24b39bacf3429b9f9
Hanna Darmawan <darmawan.hanna@gmail.com>
2022-06-05 19:10:28 +0700

    membuat 3 fungsi sort pada index.js

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+const sorter = (names, sortOrder) => {
+  let sortList = sortOrder(names);
+  let arrSortList = [];
+  for(let i=0; i<sortList.length;i++){
+    arrSortList.push(`${i + 1}. ${sortList[i]}`);
+  }
+  return arrSortList;
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
